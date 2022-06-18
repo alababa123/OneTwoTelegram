0fb44e1329aa64ae8fbe5b8e2c37d0a2b95f4763
unknown <kisell22@yandex.ru>
2022-06-18 19:57:25 +0300

    1

--- src/Components/CardDetail/CardDetail.jsx
+++ src/Components/CardDetail/CardDetail.jsx
@@ -1,49 +1,46 @@
 import React, { useState } from "react";
-import "../Card/Card.css";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import "./CardDetail.css"
 
 function CardDetail({ food, onAdd, onRemove }) {
   const [count, setCount] = useState(0);
   const cur_id = useParams();
   console.log(food);
   const { title, Image, price, id } = food[cur_id.id];
-  
+
   const handleIncrement = () => {
     setCount(count + 1);
     onAdd(food[cur_id.id]);
   };
-  
+
   const handleDecrement = () => {
     setCount(count - 1);
     onRemove(food[cur_id.id]);
   };
 
   return (
-    <div className="card">
-      <span
-        className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}
-      >
-        {count}
-      </span>
-      <div className="image__container">
-        <img src={Image} alt={title} />
+    <div className="cardDetail">
+      <div class="column">
+        <div className="image__containerDitail">
+      <img src={Image} alt={title} />
       </div>
-      <Link to={`/store/${id}`}>
-      <h4 className="card__title">
-        {title} <br/>Цена: <span className="card__price">₽{price}</span>
-      </h4>
-      </Link>
-      <div className="btn-container">
-        <Button title={"Добавить в корзину"} type={"add"} onClick={handleIncrement} />
+      </div>
+      <div class="column">
+        <h2>{title} </h2>
+        <p><span className="card__discription">{"Здесь будет описание"}</span></p>
+        <p><div className="btn-containerDetail">
+        <Button title={"+"} type={"add"} onClick={handleIncrement} />
         {count !== 0 ? (
           <Button title={"-"} type={"remove"} onClick={handleDecrement} />
         ) : (
           ""
         )}
+      </div></p>
       </div>
     </div>
+
   );
 }
 
